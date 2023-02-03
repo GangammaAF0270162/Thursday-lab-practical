98468bb42cfe4dbf1e2ac121ae0406f23cc3b2e4
GangammaAF0270162 <121918768+GangammaAF0270162@users.noreply.github.com>
2023-02-03 18:24:09 +0530

    Create Inheritanceprg.java

---
+++ Inheritanceprg.java
@@ -0,0 +1,10 @@
+Class Empyee//super class
+{
+      //instance variables 
+      long employeeid;
+      String employeeName;
+      String employeeAddress;
+      long employeephone;
+      double basicSalary;
+      double SpecialAllowance=250.80;
+      double hra=1000.50;
